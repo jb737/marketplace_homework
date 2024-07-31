da7c4a85ba47fbb90c75b766f476bb9a2b0f74cd
J Friesen <jiffybugg2@gmail.com>
2024-07-30 23:21:29 -0600

    create custom card and use it on sign up page

--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -1,9 +1,10 @@
 import { useMemo, useState } from "react";
-import { Container, Form, Row, Col, Button, } from "react-bootstrap";
+import { Form, Row, Col, Button, } from "react-bootstrap";
 import Address from "../../models/Address";
 import classes from "./SignUpPage.module.css";
 import FormInput from "../../components/FormInput/FormInput";
 import { Link, useNavigate } from "react-router-dom";
+import CustomCard from "../../components/CustomCard/CustomCard";
 
 
 const SignUpPage = (): JSX.Element => {
@@ -70,171 +71,164 @@ const SignUpPage = (): JSX.Element => {
        })
     };
 
+    const form =   <Form noValidate  validated={validated} onSubmit = {onSubmitHandler}>
+    <fieldset>
+        <Row>
+        <Col>
+        <FormInput
+            type = "text"
+            required 
+            title = "First Name"
+            value = {firstName}
+            onChange = {(e) => setFirstName(e.target.value.trim())}
+            />
+        </Col>
+        <Col>
+        <FormInput
+            type = "text"
+            required
+            title = "Last Name"
+            value = {lastName}
+            onChange={(e) => setLastName(e.target.value.trim())}
+            />
+        </Col>
+        <Col>
+        <FormInput
+            type = "date"
+            required
+            title = "Date of Birth"
+            value = {dateOfBirth ?? ""}
+            onChange={(e) => setDateOfBirth(new Date(e.target.value))}
+            max = {dateOfBirthLimit.toISOString().split("T")[0]}
+            />
+        </Col>
+        </Row>
+    </fieldset>
+
+    <fieldset>
+        <Row>
+        <Col>
+        <FormInput
+            type = "text"
+            required
+            title = "Street Number"
+            value = {address.number}
+            onChange={(e) =>  setAddress((prevState) => ({
+                ...prevState,
+                number: e.target.value.trim(),
+            }))}
+            />
+        </Col>
+        <Col>
+        <FormInput
+            type = "text"
+            required
+            title = "Street Name"
+            value = {address.street}
+            onChange={(e) =>  setAddress((prevState) => ({
+                ...prevState,
+                street: e.target.value.trim(),
+            }))}
+            />
+        </Col>
+        <Col>
+        <FormInput
+            type = "text"
+            required
+            title = "City / Town / Municipality"
+            value = {address.city}
+            onChange={(e) =>  setAddress((prevState) => ({
+                ...prevState,
+                city: e.target.value.trim(),
+            }))}
+            />
+        </Col>
+        </Row>
+    </fieldset>
+
+    <fieldset>
+        <Row>
+        <Col>
+        <FormInput
+            type = "text"
+            required
+            title = "State / Province / Territory"
+            value = {address.state}
+            onChange={(e) =>  setAddress((prevState) => ({
+                ...prevState,
+                state: e.target.value.trim(),
+            }))}
+            />
+        </Col>
+        <Col>
+        <FormInput
+            type = "text"
+            required
+            title = "Zip / Postal Code"
+            value = {address.zip}
+            onChange={(e) =>  setAddress((prevState) => ({
+                ...prevState,
+                zip: e.target.value.trim(),
+            }))}
+            />
+        </Col>
+        <Col>
+        <FormInput
+            type = "text"
+            required
+            title = "Country"
+            value = {address.country}
+            onChange={(e) =>  setAddress((prevState) => ({
+                ...prevState,
+                country: e.target.value.trim(),
+            }))}
+            />
+        </Col>
+        </Row>
+    </fieldset>
+
+    <fieldset>
+        <Row>
+        <Col>
+        <FormInput
+            type = "text"
+            required
+            title = "E-Mail"
+            value = {email}
+            onChange={(e) => setEmail(e.target.value.trim())}
+            />
+        </Col>
+        <Col>
+        <FormInput
+            type = "password"
+            required
+            title = "Password"
+            value = {password}
+            onChange={(e) => setPassword(e.target.value.trim())}
+            />
+        </Col>
+        <Col>
+        <FormInput
+            type = "password"
+            required
+            title = "Confirm Password"
+            value = {confirmPassword}
+            onChange = {(e) => setConfirmPassword(e.target.value.trim())}
+            />
+        </Col>
+        </Row>
+        {password !== confirmPassword && <Row className = {classes.error_message + " error_text"}>Passwords do not match</Row>}
+    </fieldset>
+    <div className = {classes.submit_btn_container}>
+    <Button disabled = { (password !== confirmPassword) || !email } className = {classes.submit_btn} type = "submit">Submit Registration</Button>
+    </div>
+</Form>
+
+    const footer =  <p>Already have an account?<Link to = "/login">Log In here:</Link></p>
 
     return ( 
-    <Container className = {classes.container + " center"}>
-        <Row> 
-           <h3 className={classes.title}>Sign Up</h3>
-        </Row>
 
-        <Row>
-        <Form noValidate  validated={validated} onSubmit = {onSubmitHandler}>
-            <fieldset>
-                <Row>
-                <Col>
-                <FormInput
-                    type = "text"
-                    required 
-                    title = "First Name"
-                    value = {firstName}
-                    onChange = {(e) => setFirstName(e.target.value.trim())}
-                    />
-                </Col>
-                <Col>
-                <FormInput
-                    type = "text"
-                    required
-                    title = "Last Name"
-                    value = {lastName}
-                    onChange={(e) => setLastName(e.target.value.trim())}
-                    />
-                </Col>
-                <Col>
-                <FormInput
-                    type = "date"
-                    required
-                    title = "Date of Birth"
-                    value = {dateOfBirth ?? ""}
-                    onChange={(e) => setDateOfBirth(new Date(e.target.value))}
-                    max = {dateOfBirthLimit.toISOString().split("T")[0]}
-                    />
-                </Col>
-                </Row>
-            </fieldset>
-
-            <fieldset>
-                <Row>
-                <Col>
-                <FormInput
-                    type = "text"
-                    required
-                    title = "Street Number"
-                    value = {address.number}
-                    onChange={(e) =>  setAddress((prevState) => ({
-                        ...prevState,
-                        number: e.target.value.trim(),
-                    }))}
-                    />
-                </Col>
-                <Col>
-                <FormInput
-                    type = "text"
-                    required
-                    title = "Street Name"
-                    value = {address.street}
-                    onChange={(e) =>  setAddress((prevState) => ({
-                        ...prevState,
-                        street: e.target.value.trim(),
-                    }))}
-                    />
-                </Col>
-                <Col>
-                <FormInput
-                    type = "text"
-                    required
-                    title = "City / Town / Municipality"
-                    value = {address.city}
-                    onChange={(e) =>  setAddress((prevState) => ({
-                        ...prevState,
-                        city: e.target.value.trim(),
-                    }))}
-                    />
-                </Col>
-                </Row>
-            </fieldset>
-
-            <fieldset>
-                <Row>
-                <Col>
-                <FormInput
-                    type = "text"
-                    required
-                    title = "State / Province / Territory"
-                    value = {address.state}
-                    onChange={(e) =>  setAddress((prevState) => ({
-                        ...prevState,
-                        state: e.target.value.trim(),
-                    }))}
-                    />
-                </Col>
-                <Col>
-                <FormInput
-                    type = "text"
-                    required
-                    title = "Zip / Postal Code"
-                    value = {address.zip}
-                    onChange={(e) =>  setAddress((prevState) => ({
-                        ...prevState,
-                        zip: e.target.value.trim(),
-                    }))}
-                    />
-                </Col>
-                <Col>
-                <FormInput
-                    type = "text"
-                    required
-                    title = "Country"
-                    value = {address.country}
-                    onChange={(e) =>  setAddress((prevState) => ({
-                        ...prevState,
-                        country: e.target.value.trim(),
-                    }))}
-                    />
-                </Col>
-                </Row>
-            </fieldset>
-
-            <fieldset>
-                <Row>
-                <Col>
-                <FormInput
-                    type = "text"
-                    required
-                    title = "E-Mail"
-                    value = {email}
-                    onChange={(e) => setEmail(e.target.value.trim())}
-                    />
-                </Col>
-                <Col>
-                <FormInput
-                    type = "password"
-                    required
-                    title = "Password"
-                    value = {password}
-                    onChange={(e) => setPassword(e.target.value.trim())}
-                    />
-                </Col>
-                <Col>
-                <FormInput
-                    type = "password"
-                    required
-                    title = "Confirm Password"
-                    value = {confirmPassword}
-                    onChange = {(e) => setConfirmPassword(e.target.value.trim())}
-                    />
-                </Col>
-                </Row>
-                {password !== confirmPassword && <Row className = {classes.error_message + " error_text"}>Passwords do not match</Row>}
-            </fieldset>
-            <div className = {classes.submit_btn_container}>
-            <Button disabled = { (password !== confirmPassword) || !email } className = {classes.submit_btn} type = "submit">Submit Registration</Button>
-            </div>
-        </Form>
-        </Row>
-        <Row className = {classes.footer}>
-            <p>Already have an account? </p><Link to = "/login">Log In here:</Link>
-        </Row>
-    </Container>
+        <CustomCard title = "Sign-Up" content = {form} footer = {footer} />   
 )};
 
 
