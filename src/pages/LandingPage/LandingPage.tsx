44a121f487db7d8acaee0e1293c33ad3c365c002
J Friesen <jiffybugg2@gmail.com>
2024-07-31 23:53:29 -0600

    other styling, add icon asset

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 
 import User from "../../models/User";
+import Layout from "../../components/Layout/Layout";
 
 export default function LandingPage() {
     const { state } = useLocation();
     const [user] = useState<User>(state?.user);
 
-    return user ? <div>Landing Page is under Construction</div> : <Navigate to = "/register"  />;
+    return user ?( 
+    <Layout> <div>Hello, {user.email.split("@")[0]}</div></Layout>
+     ) : (<Navigate to = "/register"  />
+);
 }
