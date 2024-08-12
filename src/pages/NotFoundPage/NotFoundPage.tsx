30fef08cf9520c2a20cca06afd3870cd3ea71e07
J Friesen <jiffybugg2@gmail.com>
2024-08-12 15:03:08 -0600

    add title, alt text for notFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -9,7 +9,7 @@ export default function NotFoundPage() {
         <div className={classes.page_container} data-theme = {theme}>
     <div className = {classes.container + " center"}>
         <h2>404</h2>
-        <img src = "src/Assets/shopping-bag.png"/>
+        <img alt = "MERN Shop logo" title = "MERN Shop logo" src = "src/Assets/shopping-bag.png"/>
         <Row><h3>Sorry, your page could not be found</h3></Row>
         <Link type = "button" to = "/">Return to Dashboard</Link>
     </div>
