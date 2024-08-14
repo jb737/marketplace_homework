33c8ac5327dd0733137ff2ca8bd4e475722a1af0
J Friesen <jiffybugg2@gmail.com>
2024-08-14 17:49:25 -0600

    add label to, change style of button

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,17 +1,18 @@
 import { Row, } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import classes from "./NotFoundPage.module.css"
-import { useState } from "react";
+import { useContext } from "react";
+import { ColorThemeContext } from "../../contexts/ColorThemeContext";
 
 export default function NotFoundPage() {
-    const [theme] = useState(window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "");
+    const { theme } = useContext(ColorThemeContext);
     return (
         <div className={classes.page_container} data-theme = {theme}>
-    <div className = {classes.container + " center"}>
+    <div data-theme = {theme} className = {classes.container + " center"}>
         <h2>404</h2>
         <img alt = "MERN Shop logo" title = "MERN Shop logo" src = "src/Assets/shopping-bag.png"/>
         <Row><h3>Sorry, your page could not be found</h3></Row>
-        <Link type = "button" to = "/">Return to Dashboard</Link>
+        <Link alt = "return to dashboard" title = "Return to Dashboard" type = "btn btn-secondary" to = "/">Return to Dashboard</Link>
     </div>
     </div>
     )
